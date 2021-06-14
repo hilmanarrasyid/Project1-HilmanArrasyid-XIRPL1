88c528efe4660cc1686978972ccf83ad91da220a
hilmanarrasyid <iamiman3419@gmail.com>
2021-06-14 21:36:16 +0700

    Implementasi Kelas Menu

--- src/classes/Menu.java
+++ src/classes/Menu.java
@@ -4,18 +4,33 @@ public class Menu {
 
     private String nama_menu;
     private double harga;
-    private String Kategori;
+    private String kategori;
 
-    public String getNama_Menu() { return ""; }
+    public String getNama_Menu(){
+        return nama_menu;
+    }
 
-    public void setNama_menu(String nama_menu) {}
+    public void setNama_menu(String nama_menu) {
+        this.nama_menu = nama_menu;
+    }
 
-    public double getHarga() { return 0; }
+    public double getHarga() {
+        return harga;
+    }
 
-    void setHarga(double harga) {}
+    void setHarga(double harga) {
+        this.harga = harga;
+    }
 
-    public String getKategori() { return ""; }
+    public String getKategori() {
+        return kategori;
+    }
 
-    public void setKategori(String kategori) {}
-}
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    String getNama_menu() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
